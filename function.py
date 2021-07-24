95aed6a36c1b5970253ba3fb36c14278b0e6f159
manivenkata <86845769+manivenkata@users.noreply.github.com>
2021-07-25 03:12:42 +0530

    function.py

---
+++ function.py
@@ -0,0 +1,12 @@
+s=input("Please enter the input:")
+
+def most_frequent(string):
+    d = dict()
+    for key in string:
+        if key not in d:
+            d[key] = 1
+        else:
+            d[key] += 1
+    return d
+
+print(most_frequent(s))
